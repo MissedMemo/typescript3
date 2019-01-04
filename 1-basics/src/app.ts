32340e5c1e7bf9d6d5b5d23561a6ebd7e0bccefa
AndyL <missedmemo@gmail.com>
2019-01-04 14:30:13 -0800

    streamline build & specify function signature

--- 1-basics/src/app.ts
+++ 1-basics/src/app.ts
@@ -11,11 +11,20 @@ someString = 42
 
 let strs: string[] = ['a', 'b', 'c' ]
 strs.push(456)
-*/
 
 let multiPass: any[] = ['a', 2, true, 'b', 3, 'c' ]
 multiPass.push( {other: 76} )
 console.log(multiPass)
+
+let someTuple: [number, string] = [24,'abc']
+*/
+
+const add = (a:number, b:number) : number => a + b
+console.log( '12 + 23 =', add(12,23) )
+
+const noReturn = (str:string) : void => {
+  console.log('not returning anything at all!')
+}
 
 let root = document.createElement('div')
 root.innerText = 'Bye!'
