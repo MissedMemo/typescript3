5575161c2832270df2a34fc8286bbcf1590d6d3a
AndyL <missedmemo@gmail.com>
2019-01-05 08:23:15 -0800

    object signature and alias

--- 1-basics/src/app.ts
+++ 1-basics/src/app.ts
@@ -21,14 +21,31 @@ let someTuple: [number, string] = [24,'abc']
 const noReturn = (str:string) : void => {
   console.log('not returning anything at all!')
 }
-*/
 
 const add = (a:number, b:number) : number => a + b
-//console.log( '12 + 23 =', add(12,23) )
+console.log( '12 + 23 =', add(12,23) )
 
 let fnReference: (a: number, b: number) => number
 fnReference = add
 console.log( '10 + 13 =', fnReference(10,13) )
+
+const objA = { name: 'Fred', age: 21 }
+const objB = { firstname: 'Tom', age: 99 }
+const objC = { name: 42, age: 21 }
+
+let objRef: { name: string, age: number }
+objRef = objA
+console.log( 'objA:', objRef )
+// objRef = objB
+// console.log( 'objB:', objRef )
+// objRef = objC
+// console.log( 'objC:', objRef )
+
+type objAlias = { name: string, age: number }
+const obj1: objAlias = { name: 'Bob', age: 24 }
+const obj2: objAlias = { name: 'Sally', age: 88 }
+
+*/
 
 let root = document.createElement('div')
 root.innerText = 'Bye!'
