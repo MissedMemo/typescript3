6e4f102cf36d200c4b03972448a490fb2a35e316
AndyL <missedmemo@gmail.com>
2019-01-06 11:04:10 -0800

    basic class element syntax variations

--- 1-basics/src/classes.ts
+++ 1-basics/src/classes.ts
@@ -1,3 +1,25 @@
+class Person {
+  name: string // defaults to public
+
+  protected isCool: boolean
+
+  private age: number
+
+  constructor( name: string, public nickname: string, age: number, isCool = true ) {
+    this.name = name
+    // nickname AUTOMATICALLY assigned, due to 'public' specifier
+    this.age = age
+    this.isCool = isCool
+  }
+}
+
+const Pete = new Person('Peter', 'Pete', 57)
+console.log('Peter person:', Pete ) // will already reflect name revision
+Pete.name = 'Joe'
+// console.log('age:', Pete.age ) inaccessible!
+// console.log('coolness:', Pete.isCool ) inaccessible!
+console.log('revised Person:', Pete )
+
 let root = document.createElement('div')
 root.innerText = 'Exploring Classes!'
 document.body.appendChild(root)
