264b3793ed271cbec1530f20cb8d1b968c3fd9e6
AndyL <missedmemo@gmail.com>
2019-01-07 20:50:26 -0800

    generics implicit type resolution

--- 1-basics/src/generics.ts
+++ 1-basics/src/generics.ts
@@ -10,6 +10,11 @@ console.log('print2<string>:', print2<string>('yowza2') )
 console.log('print1<number>:', print1<number>(1) )
 console.log('print2<number>:', print2<number>(2) )
 
+console.log('print1(someStr):', print1('yowza1') )
+console.log('print2(someStr):', print2('yowza2') )
+console.log('print1(someNum)', print1(1) )
+console.log('print2(someNum)', print2(2) )
+
 
 let root = document.createElement('div')
 root.innerText = 'Exploring Generics!'
