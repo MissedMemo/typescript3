32192f534548262b9c30f57607c40b0f2a283b28
AndyL <missedmemo@gmail.com>
2019-01-08 09:59:42 -0800

    simplest built-in generics

--- 1-basics/src/generics.ts
+++ 1-basics/src/generics.ts
@@ -1,4 +1,4 @@
-
+/*
 const print1 = <T>(arg: T): T => arg
 
 function print2<T>( arg: T ): T {
@@ -14,7 +14,15 @@ console.log('print1(someStr):', print1('yowza1') )
 console.log('print2(someStr):', print2('yowza2') )
 console.log('print1(someNum)', print1(1) )
 console.log('print2(someNum)', print2(2) )
+*/
 
+const arr1: string[] = ['One', 'Two', 'Ten']
+const arr2: Array<string> = ['Three', 'Four', 'Twenty']
+const arr3: ReadonlyArray<string> = ['Five', 'Six', 'One Hundred']
+
+console.log( 'arr1:', arr1 )
+console.log( 'arr2:', arr2 )
+console.log( 'arr3:', arr3 )
 
 let root = document.createElement('div')
 root.innerText = 'Exploring Generics!'
