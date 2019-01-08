7305d641c7b9aac651e32b86f3ea31ffb72fd503
AndyL <missedmemo@gmail.com>
2019-01-08 10:37:59 -0800

    add constraints to generic types

--- 1-basics/src/generics.ts
+++ 1-basics/src/generics.ts
@@ -14,7 +14,6 @@ console.log('print1(someStr):', print1('yowza1') )
 console.log('print2(someStr):', print2('yowza2') )
 console.log('print1(someNum)', print1(1) )
 console.log('print2(someNum)', print2(2) )
-*/
 
 const arr1: string[] = ['One', 'Two', 'Ten']
 const arr2: Array<string> = ['Three', 'Four', 'Twenty']
@@ -23,6 +22,42 @@ const arr3: ReadonlyArray<string> = ['Five', 'Six', 'One Hundred']
 console.log( 'arr1:', arr1 )
 console.log( 'arr2:', arr2 )
 console.log( 'arr3:', arr3 )
+*/
+
+// Add constraints... 
+
+function logRtnX1<T>( arg: T[] ): T[] {
+  console.log(arg.length)
+  return arg
+}
+
+function logRtnX2<T>( arg: Array<T> ): Array<T> {
+  console.log(arg.length)
+  return arg
+}
+
+logRtnX1([1,2,3])
+// logRtnX1(24) - not an array!
+// logRtnX2('abc') - not an array!
+
+
+interface IHasLength {
+  length: number
+}
+
+function logRtn1<T extends IHasLength>( arg: T ): T {
+  console.log(arg.length)
+  return arg
+}
+
+const logRtn2 = <T extends IHasLength>(arg: T): T => {
+  console.log(arg.length)
+  return arg
+}
+
+logRtn1([1,2,3])
+logRtn1('abc')
+//logRtn1(24)
 
 let root = document.createElement('div')
 root.innerText = 'Exploring Generics!'
